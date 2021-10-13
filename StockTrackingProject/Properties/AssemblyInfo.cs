b95cdecee8297db621ab6659adc0b16fdd87dd92
bella84 <110831377+bella84@users.noreply.github.com>
2021-10-13 12:58:52 +0000

    Linux 3.10.63

--- StockTrackingProject/Properties/AssemblyInfo.cs
+++ StockTrackingProject/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StockTrackingProject")]
-[assembly: Assemb
+[assembly: Assembly
