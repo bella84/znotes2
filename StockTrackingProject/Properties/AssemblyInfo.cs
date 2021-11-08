286615977f8c3f0557b8762b9d706bb479c57e3b
bella84 <110831377+bella84@users.noreply.github.com>
2021-11-08 22:12:37 +0000

    hmm

--- StockTrackingProject/Properties/AssemblyInfo.cs
+++ StockTrackingProject/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StockTrackingProject")]
-[assembly: AssemblyCopyright("Copyright ©  
+[assembly: AssemblyCopyright("Copyright ©  20
