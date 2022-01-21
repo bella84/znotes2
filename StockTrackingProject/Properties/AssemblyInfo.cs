de7888a342784cf7843aef632075cffdbfbb012e
bella84 <110831377+bella84@users.noreply.github.com>
2022-01-21 15:03:26 +0000

    CV update

--- StockTrackingProject/Properties/AssemblyInfo.cs
+++ StockTrackingProject/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the
+// Setting ComVisible to false makes the t
