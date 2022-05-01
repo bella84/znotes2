4541c0379c983be770df023829d96e825a4b0f58
bella84 <110831377+bella84@users.noreply.github.com>
2022-05-01 13:06:51 +0000

    Linux 3.10.86

--- StockTrackingProject/Properties/AssemblyInfo.cs
+++ StockTrackingProject/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you ne
+// to COM components.  If you need
