30330b2a7c0c945181b6ec5fc7cf207a3ffc2a19
bella84 <110831377+bella84@users.noreply.github.com>
2022-08-05 11:30:31 +0000

    remove badge

--- StockTrackingProject/Properties/AssemblyInfo.cs
+++ StockTrackingProject/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for 
+// The following GUID is for t
